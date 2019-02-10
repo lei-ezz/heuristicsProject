b2521ee86615dbf9ce524222329ff61a6c042752
donald108 <__@live.ca>
2019-02-10 11:50:18 +0000

    Deleted not needed files

--- Website/helper.py
+++ Website/helper.py
@@ -1,21 +1,5 @@
 from flask import Response
-import mysql.connector
 import json
-
-def start (): 
-    connect = mysql.connector.connect(
-        user = '<username-here>', 
-        password = '<password-here>',
-        host = '<database-host-here>',
-        database = '<database-name-here>', 
-        buffered = True
-    )
-    cursor = connect.cursor()
-    return connect, cursor
-
-def stop (con, cursor):
-    cursor.close()
-    con.close()
 
 def error():
     return Response(json.dumps({'error' : 'an error occured'}), status=404, mimetype='application/json')
--- Website/web.py
+++ Website/web.py
@@ -1,6 +1,5 @@
 # For flask, mysql, crypto
 from flask import current_app, Blueprint, render_template, send_from_directory
-import lounger
 import os
 
 # Setup the get blueprint
@@ -11,23 +10,6 @@ web = Blueprint('web', __name__)
 def index():
     """Routes '/' to index.html"""
     return render_template('index.html')
-
-# More browser endpoints here
-@web.route('/loungers', methods=['GET'])
-def loungers():
-    #We'll need the number of loungers, etc
-    l = (5, ) * lounger.getNumLounger()
-    return render_template('lounger.html', loungers=l, number=lounger.getNumLounger())
-
-@web.route('/orders', methods=['GET'])
-def orders():
-    #We'll need the number of loungers, etc
-    return render_template('drink_selection.html')
-
-@web.route('/contacts', methods=['GET'])
-def contacts():
-    #We'll need the number of loungers, etc
-    return render_template('contact.html')
 
 # For downloading files
 @web.route('/files/<path:filename>', methods=['GET'])
