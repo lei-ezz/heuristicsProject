a671ac65c54d91b708c96835104f5435c6e75736
Hanalei Ezz <39172959+lei-ezz@users.noreply.github.com>
2019-02-14 17:52:12 +0000

    timer ???

--- Website/static/js/timer.js
+++ Website/static/js/timer.js
@@ -5,84 +5,34 @@ $(document).ready(
   a.start();
 })
 
-var Stopwatch = function(elem, options) {
+var Stopwatch = function(elem) {
   var timer       = createTimer(),
       offset,
       clock,
       interval;
-  
-  // default options
-  options = options || {};
-  options.delay = options.delay || 1;
  
   // append elements     
   elem.appendChild(timer);
-  elem.appendChild(startButton);
-  elem.appendChild(stopButton);
-  elem.appendChild(resetButton);
-  
-  // initialize
-  reset();
   
   // private functions
   function createTimer() {
     return document.createElement("span");
-    startTiming();
   }
 
-  function startTiming() {
-    offset   = Date.now();
-    interval = setInterval(update, options.delay);
-
-    var a = document.createElement("a");
-
-    a.href = "#" + action;
-    a.innerHTML = action;
-    a.addEventListener("click", function(event) {
-      handler();
-      event.preventDefault();
-    });
-    return a;
-  }
-  
-  function createButton(action, handler) {
-    
-    var a = document.createElement("a");
-    a.href = "#" + action;
-    a.innerHTML = action;
-    a.addEventListener("click", function(event) {
-      handler();
-      event.preventDefault();
-    });
-    return a;
-  }
-  
   function start() {
     if (!interval) {
       offset   = Date.now();
-      interval = setInterval(update, options.delay);
+      interval = setInterval(update, 1);
     }
   }
-  
-  function stop() {
-    if (interval) {
-      clearInterval(interval);
-      interval = null;
-    }
-  }
-  
-  function reset() {
-    clock = 0;
-    render(0);
-  }
-  
+
   function update() {
     clock += delta();
     render();
   }
   
   function render() {
-    timer.innerHTML = clock/1000; 
+    timer.innerHTML = clock / 1000; 
   }
   
   function delta() {
@@ -95,6 +45,4 @@ var Stopwatch = function(elem, options) {
   
   // public API
   this.start  = start;
-  this.stop   = stop;
-  this.reset  = reset;
 };
