886591203de2240a2cdff31146a2e5d9a94bc35f
Hanalei Ezz <39172959+lei-ezz@users.noreply.github.com>
2019-02-14 21:37:08 +0000

    timer f-i-x-e-d with an API

--- Website/static/js/timer.js
+++ Website/static/js/timer.js
@@ -1,48 +1,8 @@
-$(document).ready(
-  function startTiming() {
-  var a = document.getElementById("timer");
-  a = new Stopwatch(a);
-  a.start();
-})
-
-var Stopwatch = function(elem) {
-  var timer       = createTimer(),
-      offset,
-      clock,
-      interval;
- 
-  // append elements     
-  elem.appendChild(timer);
-  
-  // private functions
-  function createTimer() {
-    return document.createElement("span");
-  }
-
-  function start() {
-    if (!interval) {
-      offset   = Date.now();
-      interval = setInterval(update, 1);
+$(document).ready(function () {
+  $("#timer").stopwatch().bind('tick.stopwatch', function (e, elapsed) {
+    if (elapsed >= 20001) {
+      nextImage(elapsed, 0);
+      $(this).stopwatch('reset');
     }
-  }
-
-  function update() {
-    clock += delta();
-    render();
-  }
-  
-  function render() {
-    timer.innerHTML = clock / 1000; 
-  }
-  
-  function delta() {
-    var now = Date.now(),
-        d   = now - offset;
-    
-    offset = now;
-    return d;
-  }
-  
-  // public API
-  this.start  = start;
-};
+  }).stopwatch('start')
+});
